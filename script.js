22fef400404e73131f8f5ff06bcace7f0f63ac36
cyril-bonneau <cyril.bonneau@ynov.com>
2017-05-10 15:22:17 +0200

    n

--- script.js
+++ script.js
@@ -2,11 +2,14 @@ function machineASous(){
 
     var creditJoueur = document.getElementById("monnaie").value;
     var tentativeJoueur = document.getElementById("tentative").value;
+    var highscore = document.getElementById("highscore").value;
+
     var audio = new Audio('son/pieces-1.mp3');
     var audio2 = new Audio('son/macron.mp3');
     var audio3 = new Audio('son/marine.mp3');
     var audio4 = new Audio('son/fillon.mp3');
-    var audio5 = new Audio('son/mmelenchon.mp3');
+    var audio5 = new Audio('son/melenchon.mp3');
+    var audio6 = new Audio('son/defaite.mp3');
 
 
 
@@ -21,45 +24,60 @@ function machineASous(){
     document.getElementById("img_3").src = 'img/img_' + y + '.png';
     document.getElementById("img_4").src = 'img/img_' + z + '.png';
 
-    creditJoueur -= 1;
+    
+    creditJoueur --;
     tentativeJoueur ++;
 
 
-    
+
     if(w == 1 && x == 1 && y == 1 && z == 1)
     {
         creditJoueur += 5;
         audio2.play();
     }
-    
+
     if(w == 2 && x == 2 && y == 2 && z == 2)
     {
         creditJoueur += 5;
         audio3.play();
     }
-    
+
     if(w == 3 && x == 3 && y == 3 && z == 3)
     {
         creditJoueur += 5;
         audio4.play();
     }
-    
+
     if(w == 4 && x == 4 && y == 4 && z == 4)
     {
         creditJoueur += 5;
         audio5.play();
     }
 
+    
+    if(creditJoueur > highscore)
+    {
+        highscore = creditJoueur;
+    }
+    
+    if(creditJoueur > highscore)
+    {
+        highscore = creditJoueur;
+    }
+
+
     if(creditJoueur == 0)
     {
-		document.getElementById("jouer").disabled = "disabled";
+        document.getElementById("jouer").disabled = true;
+        audio6.play();
         alert("Dommage ! Tout tes sous sont partis dans les caisses de l'état ! ");
-        
-	}
-    
+        document.getElementById("rejouer").disabled = false;
+    }
 
 
     document.getElementById("monnaie").value = creditJoueur;
     document.getElementById("tentative").value = tentativeJoueur;
-
+    document.getElementById("highscore").value = highscore;
+    storage.setItem("highscore", highscore);
+    
 }
